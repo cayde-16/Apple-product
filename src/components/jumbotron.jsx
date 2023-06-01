d2eee63eb87cf41a71ae2ef7904efdc17ace4b64
Cade Alexander Heck <cadeheck16@gmail.com>
2023-06-01 13:22:22 -0400

    started working on jumbotron

---
+++ src/components/jumbotron.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return(
+        <div className="jumbotron-section wrapper">
+            <div>
+                <h2 className="title">New</h2>
+                <h2>iPhone 14 Pro</h2>
+                <h1>Big and bigger</h1>
+                <p>From $41.62/mo. for 24 mo. or $999 before trade-in.</p>
+            </div>
+
+            <div>
+                <button>Buy</button>
+                <a href="">Learn more</a>
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Jumbotron
