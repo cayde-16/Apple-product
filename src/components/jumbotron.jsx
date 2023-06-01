5735e6b70d580bd415a677bba4c083c7e014b397
Cade Alexander Heck <cadeheck16@gmail.com>
2023-06-01 14:07:11 -0400

    updated jumbosection with text,buttons,images

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
+import iphoneTxt from '../assets/images/iphone-14.jpg';
+import iphoneInHand from '../assets/images/iphone-hand.png';
 const Jumbotron = () => {
     return(
         <div className="jumbotron-section wrapper">
-            <div>
                 <h2 className="title">New</h2>
-                <h2>iPhone 14 Pro</h2>
-                <h1>Big and bigger</h1>
-                <p>From $41.62/mo. for 24 mo. or $999 before trade-in.</p>
+                <img className="logo" src={iphoneTxt} alt="iPhone 14 Pro"/>
+                <p className="text">Big and bigger.</p>
+                <p className="description">
+                    From $41.62/mo. for 24 mo. or $999 before trade-in
+                </p>
+            <div className="links">
+                <button className="button">Buy</button>
+                <a className="link" href="/">Learn more</a>
             </div>
-
-            <div>
-                <button>Buy</button>
-                <a href="">Learn more</a>
-            </div>
-
             
+            <img className="iphone-img" src={iphoneInHand} alt="iPhone" />
+
         </div>
     )
 }
