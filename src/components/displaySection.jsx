e751084dc54c2a7549ad945f5270bf5a3b25e4f0
Cade Alexander Heck <cadeheck16@gmail.com>
2023-06-02 14:06:37 -0400

    finshed working on display section

--- src/components/displaySection.jsx
+++ src/components/displaySection.jsx
@@ -13,13 +13,18 @@ const Displaysection = () => {
 
     }
 
+    const grabCoords = (e) => {
+        console.log(`PageX:${e.pageX}, PageY:${e.pageY}`)
+        console.log(`ScreenX:${e.screenX}, ScreenY:${e.screenY}`)
+        console.log(`ClientX:${e.clientX}, ClientY:${e.clientY}`)
+    }
     return(
-        <div className="display-section wrapper">
+        <div onClick={grabCoords} className="display-section wrapper">
             <h2 className="title">New</h2>
             <h1 className="text">Brilliant.</h1>
             <p className="description">A display that's up to 2x brighter in the sun.</p>
-            <button className="button">Try me!</button>
-            <button onClick={handleScroll} className="back-button">TOP</button>
+            <button id="landing-display" className="button">Try me!</button>
+            <button  onClick={handleScroll} className="back-button">TOP</button>
         </div>   
     )
 }
